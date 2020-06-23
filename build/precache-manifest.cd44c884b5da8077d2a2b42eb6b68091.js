1ce5c427c15034ad8ba7736965fd256774464793
melissa <nadia.mm.mariduena@gmail.com>
2020-06-23 11:15:39 +0200

    json file update

---
+++ build/precache-manifest.cd44c884b5da8077d2a2b42eb6b68091.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c43dc45b50df27a5c00d486661b30248",
+    "url": "/axios-react-one/index.html"
+  },
+  {
+    "revision": "ce5b874f005e58153456",
+    "url": "/axios-react-one/static/css/main.fd52757a.chunk.css"
+  },
+  {
+    "revision": "0db3591ab1c2365f2aba",
+    "url": "/axios-react-one/static/js/2.02a32ae9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/axios-react-one/static/js/2.02a32ae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce5b874f005e58153456",
+    "url": "/axios-react-one/static/js/main.ceb286df.chunk.js"
+  },
+  {
+    "revision": "68855b36b653e5ceb323",
+    "url": "/axios-react-one/static/js/runtime-main.b981f248.js"
+  }
+]);
--- build/static/js/main.f2bee392.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpcountry-app-axios"]=this["webpackJsonpcountry-app-axios"]||[]).push([[0],[function(p,o,n){p.exports=n(1)},function(p,o){}],[[0,1]]]);
-//# sourceMappingURL=main.f2bee392.chunk.js.map
